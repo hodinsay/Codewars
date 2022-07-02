a56e83c5b463012ce691130c5bb27be43e2a948c
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-07-01 23:12:57 -0700

    Add files via upload

---
+++ 8-kyu/convertNumToString.js
@@ -0,0 +1,20 @@
+// Convert a Number to a String
+
+// Instructions:
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+
+// Solution:
+
+function numberToString(num) {
+    return num.toString();
+  }
+
+// Short Solution:
+
+const numberToString = (num) => num.toString();
