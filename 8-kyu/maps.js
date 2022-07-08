cf53e187784d412fe63715b15cc002f6a7d987a7
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-07-07 17:43:19 -0700

    Add files via upload

---
+++ 8-kyu/maps.js
@@ -0,0 +1,21 @@
+// Beginner - Lost Without a Map
+
+// Instructions:
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// Topics:
+// map
+
+// Solution:
+
+function maps(x){
+    return x.map(number => number * 2);
+}
+
+// Short Solution: 
+
+const maps = (x) => x.map(number => number * 2);
