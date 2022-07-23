6266e12363ff23c3f1c1aa5bf48e44397602d0ef
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-07-22 22:55:19 -0700

    Removing customElements

---
+++ 8-kyu/removeEveryOther.js
@@ -0,0 +1,21 @@
+// Removing customElements
+
+// Instructions:
+
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Example:
+
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+
+// Solution:
+
+function removeEveryOther(array){
+    return array.filter((_, i) => i % 2 === 0);
+}
+
+// Short Solution:
+
+const removeEveryOther = (array) => array.filter((_, i) => i % 2 === 0);
