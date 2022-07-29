08fcf4a79287ee0f2b0bfe616f3e17414bda5dad
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-07-28 23:39:11 -0700

    Function 1 - hello world!

---
+++ 8-kyu/helloWorld.js
@@ -0,0 +1,18 @@
+// Function 1 - hello world 
+
+// Descriptions:
+
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Topics:
+// Fundamental
+
+// Solution: 
+
+function greet(){
+    return 'hello world!';
+  }
+
+// Short Solution:
+
+const greet = () => 'hello world!';
